3ed1d064b5b5d0cc4e262063f3a6d3b4d26dff33
Fabio Ribeiro Martins <f.ribeiro.martins@avanade.com>
2023-02-17 15:27:11 -0300

    Injected reflect-metadata and file thar controls dependency injection

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,8 @@
+import "reflect-metadata";
 import express from "express";
 import swaggerUi from "swagger-ui-express";
+
+import "./shared/container";
 
 import { createConnection } from "./database";
 import { router } from "./routes";
